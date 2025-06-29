e5bef1021074c9d485551d783def1aae4ac7f8b0
popoyote <75493535+popoyote@users.noreply.github.com>
2025-06-29 19:49:12 +0300

    Update files.py
    adding ForeignKey from users "user_id"

--- backend/api/models/files.py
+++ backend/api/models/files.py
@@ -5,5 +5,6 @@ from backend.api.models.base import Base
 class File(Base):
     __tablename__ = 'files'
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
     name = Column(String(50))
     is_load = Column(Boolean, default=False)
